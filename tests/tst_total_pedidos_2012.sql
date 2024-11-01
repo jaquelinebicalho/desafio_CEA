6bcae1f5c1e6d41f5905766daa4c597f53bd1fc5
Jaqueline Souza <souzajaqb@gmail.com>
2024-11-01 21:02:07 +0000

    criado teste de quantidade de vendas realizadas em 2012, que garante a conformidade com os dados do database original

---
+++ tests/tst_total_pedidos_2012.sql
@@ -0,0 +1,23 @@
+
+ /*  
+    Este teste garante que a quantidade de pedidos realizados em 2012 estão
+    em conformidade com os dados encontrados no database adw: 3.915
+    
+*/
+
+{{ config (
+        severity = 'error'
+    )
+}}
+ 
+ with
+    qnt_pedidos_2012 as (
+        select 
+            COUNT(DISTINCT "Nota Fiscal") as total_pedidos
+        from {{ ref('int_vendas_enriquecida') }}
+        where "Data Emissão Venda" between '2012-01-01' and '2012-12-31'
+        --3915
+    )
+select total_pedidos
+from qnt_pedidos_2012
+where total_pedidos = 3915
